3a155754b43148dc7105f326b7e0ba73d88396d1
Jhonnatan Flores Miramon <srjhonxy@gmail.com>
2020-10-04 11:47:03 -0500

    Curso

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/styles.scss';
+import Curso from './Curso';  
+import data from './data';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const cursos = data;
+
+const App =()=> {
+ return( <>
+   <div className="main-banner img-container l-section" id="main-banner">
+  <div className="ed-grid lg-grid-6">
+    <div className="lg-cols-4 lg-x-2">
+      <img className="main-banner__img" alt="banner" src="https://i.pinimg.com/564x/c4/5e/37/c45e3755540f60cfe8c7114d36309ee2.jpg"/>
+      <div className="main-banner__data s-center">
+        <p className="t2 s-mb-0">Título del banner</p>
+        <p> Subtítulo del banner</p>
+        <a href="#" className="button">Botón del banner</a>
+      </div>
     </div>
-  );
+  </div>
+</div>
+
+<div className="ed-grid m-grid-3">
+{
+  cursos.map(curso => 
+  <Curso  
+  title = {curso.title}
+  image = {curso.image}
+  imagecard = {curso.imagecard}
+  profesor = {curso.profesor}
+  price = {curso.price}
+  />
+
+
+  )
+}
+</div>
+
+  </>)
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,28 @@
+const data = [
+    {
+    title:"Vue JS desde cero",
+    image:"https://pm1.narvii.com/6303/75dec364d3e4740373016f1712650a12de7e8cf5_128.jpg", 
+    imagecard:"https://ed-grid.com/assets/img/logo/edgrid-poster.jpg",
+    price:"20USD",
+    profesor:"Jhonnatan Flores"
+    },
+
+    {
+    title:"Go desde cero",
+    image:"https://pm1.narvii.com/6303/75dec364d3e4740373016f1712650a12de7e8cf5_128.jpg", 
+    imagecard:"https://edteam-media.s3.amazonaws.com/courses/medium/e737f2e3-92b3-400e-8d82-1b19e0277012.png",
+    price:"30USD",
+    profesor:"Jhonnatan Flores"
+    },
+
+    {
+    title:"Motion Graphics desde cero",
+    image:"https://pm1.narvii.com/6303/75dec364d3e4740373016f1712650a12de7e8cf5_128.jpg", 
+    imagecard:"https://edteam-media.s3.amazonaws.com/courses/medium/a027fe1a-fcf4-4a1c-8717-c3d52d5f9879.jpg",
+    price:"40USD",
+    profesor:"Jhonnatan Flores"
+    }
+    
+    ]
+
+    export default data;
